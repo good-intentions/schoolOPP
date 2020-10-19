a3852d17d089d730c30e9925bd169755ef987c33
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-10-19 14:06:26 +0300

    Add files via upload

---
+++ KrugTest.java
@@ -0,0 +1,12 @@
+package simeon;
+
+public class KrugTest {
+	public static void main(String[] args) {
+		
+		Circle circle1 = new Circle();
+		System.out.ptintln("Liceto na krug s r= " + circle12.r + " e ravno na " + circle1.getArea() );
+		
+		
+		
+	}
+}
