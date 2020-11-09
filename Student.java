be602b973377d024a94bb3cdb7f46362178ee508
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-11-09 13:46:27 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+package simeon;
+
+public class Student {
+	public String name;
+	public double grade;
+	
+	public Student() {
+		
+	}
+
+	public Student(String name, double grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+}
