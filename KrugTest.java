dc155b409ae6729e6a0b2a596d4943b50d459c29
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-11-26 14:49:06 +0200

    Update KrugTest.java

--- KrugTest.java
+++ KrugTest.java
@@ -4,7 +4,7 @@ public class KrugTest {
 	public static void main(String[] args) {
 		
 		Circle circle1 = new Circle();
-		System.out.ptintln("Liceto na krug s r= " + circle12.r + " e ravno na " + circle1.getArea() );
+		System.out.ptintln("Liceto na krug s r= " + circle1.r + " e ravno na " + circle1.getArea() );
 		
 		
 		
