7bc195740d362a93912f807ed1bdd82b0b0a401c
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-11-26 14:47:18 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,25 @@
 package simeon;
 
 public class Circle {
-
+	public double radius;
+	
+	public Circle() {
+		radius = 1; //default
+	}
+	
+	public Circle(double newRadius) {
+		radius = newRadius;
+	}
+	
+	public double getArea() {
+		return Math.PI*radius*radius;
+	}
+	
+	public double getPerimeter() {
+		return Math.PI*radius*2;
+	}
+	
+	public void setRadius(double newRadius) {
+		radius = newRadius;
+	}
 }
